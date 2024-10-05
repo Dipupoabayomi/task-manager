2737de597ccbed3411293a2ac356c08a935a7b41
Dipupo <khalilulah.abayomi@gmail.com>
2024-10-05 13:50:51 +0100

    cors added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const colors = require('colors');
-
+const cors = require('cors');
 
 const  app = express();
 dotenv.config({path: ".env"});
 connectDB();
 app.use(express.json())
+
+app.use(cors())
 
 const task = require('./routes/task');
 
